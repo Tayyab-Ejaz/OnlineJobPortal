1e0c49bf653be499040e20b861676b2aa2fa932e
tayyabejaz668 <tayyabejaz668@gmail.com>
2021-06-03 06:52:01 +0500

    Add project files.

---
+++ OnlineJobPortal/Client/wwwroot/Script.js
@@ -0,0 +1,15 @@
+﻿
+
+
+window.AssignFunctionality = function () {
+    $(".collapse.show").each(function () {
+        $(this).prev(".card-header").find(".fa").addClass("fa-angle-down").removeClass("fa-angle-right");
+    });
+
+    // Toggle right and down arrow icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function () {
+        $(this).prev(".card-header").find(".fa").removeClass("fa-angle-right").addClass("fa-angle-down");
+    }).on('hide.bs.collapse', function () {
+        $(this).prev(".card-header").find(".fa").removeClass("fa-angle-down").addClass("fa-angle-right");
+    });
+}
