74bf8cd5f88ca024f1274d85452f99b4e11ab0fd
Paradox <mbilalasif@icloud.com>
2021-06-05 01:12:08 +0500

    Autherization/role/claim

--- OnlineJobPortal/Server/Startup.cs
+++ OnlineJobPortal/Server/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -9,12 +10,34 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OnlineJobPortal.Server.Data;
 using OnlineJobPortal.Server.Models;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace OnlineJobPortal.Server
 {
     public class Startup
     {
+        private void CreateUser(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string email = "waqar@gmail.com";
+            string securePassword = "Pa$$w0rd!";
+            Task<ApplicationUser> adminUser = userManager.FindByEmailAsync(email);
+            adminUser.Wait();
+            if (adminUser.Result == null)
+            {
+                var admin = new ApplicationUser();
+                admin.Email = email;
+                admin.UserName = email;
+                Task<IdentityResult> newUser = userManager.CreateAsync(admin, securePassword);
+                newUser.Wait();
+            }
+            var createdAdminUser = userManager.FindByEmailAsync(email);
+            createdAdminUser.Wait();
+            userManager.AddClaimAsync(createdAdminUser.Result,
+                new System.Security.Claims.Claim("role", "Admin")).Wait();
+        }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,27 +49,43 @@ namespace OnlineJobPortal.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+           
+
+            services.AddDefaultIdentity<ApplicationUser>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+
+            }).AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(options => { options.IdentityResources["openid"].UserClaims.Add("role");
+                    options.ApiResources.Single().UserClaims.Add("role");
+                });
+            System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("role");
             services.AddAuthentication()
                 .AddIdentityServerJwt();
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IServiceProvider sp)
         {
             if (env.IsDevelopment())
             {
@@ -77,6 +116,7 @@ namespace OnlineJobPortal.Server
                 endpoints.MapControllers();
                 endpoints.MapFallbackToFile("index.html");
             });
+            CreateUser(sp);
         }
     }
 }
